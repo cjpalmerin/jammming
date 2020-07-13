ef69c35a4196e2a804bc6ab724b1b9da8e52f34d
CJPALME <cjpalmerin@gmail.com>
2020-07-13 17:11:53 -0500

    added paylist, searchbar, search results, track, and tracklist components.

---
+++ client/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './SearchResults.css'
+
+function SearchResults() {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+           {/*Add a TrackList component*/}
+        </div>
+    );
+}
+
+export default SearchResults;
---
+++ client/src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './TrackList.css'
+
+function TrackList() {
+    return (
+        <div class="TrackList">
+            {/*You will add a map method that renders a set of Track components*/}
+        </div>
+    );
+}
+
+export default TrackList;
