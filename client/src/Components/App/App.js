72605f19d6862f92e09abc3126f4ac96f19b12aa
CJPALME <cjpalmerin@gmail.com>
2020-07-14 15:01:31 -0500

    rendered hard coded playlist data

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }]
+      searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }],
+      playlistName: "Playlist 1",
+      playlistTracks: [{ name: "name1", artist: "artist1", album: "album1", id: 4 }, { name: "name2", artist: "artist2", album: "album2", id: 5 }, { name: "name3", artist: "artist3", album: "album3", id: 6 }]
     }
   }
   render() {
@@ -23,7 +25,7 @@ class App extends React.Component {
             {/* Add a SearchResults component */}
             <SearchResults searchResults={this.state.searchResults} />
             {/* Add a Playlist component */}
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
